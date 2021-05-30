f169f04c80808c6b024aa47c316a55931c1633dd
yoshito <yoshito5211@gmail.com>
2021-05-31 00:10:47 +0900

    ruby基礎課題Q1~Q5を回答

--- task.rb
+++ task.rb
@@ -5,6 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.push("斎藤")
+  p names
 
 end
 
@@ -13,6 +15,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
+  p array = array1 + array2
 
 end
 
@@ -20,6 +23,7 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
 
 end
 
@@ -27,7 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.delete(nil)
   # 以下は変更しないで下さい
   p sports
 end
@@ -37,6 +41,15 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  arrays = [array1, array2]
+  
+  arrays.each do |array|
+    if array.empty?
+      puts true
+    else
+      puts false
+    end
+  end
 
 end
 
