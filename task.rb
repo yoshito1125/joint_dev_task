d9cc7d76d3cec74a8dc6e19ac3dbcfe7eae0d44b
yoshito <yoshito5211@gmail.com>
2021-06-01 12:38:40 +0900

    Q1,Q4,Q5を修正

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names << "斎藤"
   p names
 
 end
@@ -31,7 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -42,13 +42,9 @@ def q5
 
   # 以下に回答を記載
   arrays = [array1, array2]
-  
+
   arrays.each do |array|
-    if array.empty?
-      puts true
-    else
-      puts false
-    end
+    p array.empty?
   end
 
 end
