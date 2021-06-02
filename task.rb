48291976ffd6ef81d6bb1ae09664441a47307eba
yoshito <yoshito5211@gmail.com>
2021-06-03 00:45:08 +0900

    Ruby課題(基礎)Q6~Q8を修正

--- task.rb
+++ task.rb
@@ -53,7 +53,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers2 = numbers1.map {|x| x * 10}
+  numbers2 = numbers1.map {|num| num * 10}
   p numbers2
 
 end
@@ -62,7 +62,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array.map! {|x| x.to_i}
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -72,6 +72,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
