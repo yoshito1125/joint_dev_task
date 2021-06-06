9268f6746796618252a0505df2c8cfdc088c7ada
yoshito <yoshito5211@gmail.com>
2021-06-06 14:15:12 +0900

    Ruby課題(応用)Q17〜Q20を回答

--- task.rb
+++ task.rb
@@ -173,6 +173,20 @@ end
 class UserQ17
   # 以下に回答を記載
 
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def info
+    puts <<~TEXT
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    TEXT
+  end
+
 end
 
 def q17
@@ -188,6 +202,19 @@ end
 class UserQ18
   # 以下に回答を記載
 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    if @age > 30
+      puts "こんにちは、#{@name}と申します。宜しくお願いいたします。"
+    else
+      puts "はいさいまいど〜、#{@name}です!!!"
+    end
+  end
+
 end
 
 def q18
@@ -201,8 +228,9 @@ end
 
 class Item
   # 以下を修正して下さい
+  attr_reader :name
 
-  def initialize(name)
+  def initialize(name:)
     @name = name
   end
 end
@@ -215,12 +243,34 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_reader :name, :age
 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(name:, entry_fee:)
+    @name = name
+    @entry_fee = entry_fee
+  end
 
+  def info_entry_fee(user)
+    entry_fee = case user.age
+    when 0..5
+      @entry_fee[:infant]
+    when 6..12
+      @entry_fee[:children]
+    when 13..64
+      @entry_fee[:adult]
+    when 65..120
+      @entry_fee[:senior]
+    end
+    puts "#{user.name}さんの入場料金は#{entry_fee}円です。"
+  end
 end
 
 
